4a21f9c352749c298c0a6ba526276b1f7f23a60b
Braden Jonsson <braden337@gmail.com>
2016-08-24 18:59:01 -0500

    example 5

---
+++ ex5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  cout << setw(9) << "horses";
+  cout << setw(9) << "pigs";
+  cout << setw(9) << "rabbits" << endl;
+
+  for (int h = 0; h < 100; h++) {
+    for (int p = 0; p < 100; p++) {
+      for (int r = 0; r < 100; r++) {
+        if (h + p + r == 100 && h*10 + p*3 + r*0.5 == 100) {
+          cout << setw(9) << h;
+          cout << setw(9) << p;
+          cout << setw(9) << r << endl;
+        }
+      }
+    }
+  }
+
+
+}
