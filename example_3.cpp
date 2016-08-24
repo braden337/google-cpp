bd2218314b15c8a0daccd06ec5c23a93c5a21d15
Braden Jonsson <braden337@gmail.com>
2016-08-24 19:55:58 +0000

    example 4

--- example_3.cpp
+++ example_3.cpp
@@ -3,11 +3,18 @@
 using namespace std;
 
 int main() {
-  cout << setiosflags(ios::right | ios::setw(2));
+  cout << setiosflags(ios::right);
   // cout << "    1\t2\t3\t4\t5\t6\t7\t8\t9" << endl;
   for (int x = 1; x < 10; x++) {
-    cout << x;
+    int width = 0;
+    if (x == 1) {
+      width = 5;
+    } else {
+      width = 2;
+    }
+    cout << setw(width) << x << '\t';
   }
+  
   cout << endl;
   for (int c = 1; c < 10; c++) {
     cout << c << "| ";
---
+++ example_4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main() {
+    
+    srand(time(NULL));
+    int secret = rand() % 100;
+    int guess;
+    
+    cout << "Guess a number between 0 and 100"<< endl;
+    
+    do {
+        
+        while (!(cin >> guess)) {
+            cout << "Only numbers please" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    
+        if (secret < guess) {
+            cout << "Too high" << endl;
+        } else if (secret > guess) {
+            cout << "Too low" << endl;
+        } else {
+            cout << "Congratulations" << endl;
+        }
+    
+    } while(!(guess == secret));
+    
+}
